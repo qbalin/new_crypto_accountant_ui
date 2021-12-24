159710caa60fa1c1d765994800cc947e07e805f1
Quentin Balin <quentin.balin@gusto.com>
2021-12-24 10:01:07 -0500

    Use next.js

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Vectigalia</title>
+      </Head>
+
+      <main>
+      </main>
+
+      <style jsx>{`
+      `}</style>
+    </div>
+  )
+}
